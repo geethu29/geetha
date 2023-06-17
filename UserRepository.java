827bf33df1941d6d2a77cb8bfddd192d7ba1cc43
geethu29 <132564906+geethu29@users.noreply.github.com>
2023-06-17 12:41:03 +0530

    Add files via upload

---
+++ UserRepository.java
@@ -0,0 +1,16 @@
+package com.example.demo.repositories;
+
+import com.example.demo.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    @Query(value = "SELECT u FROM User u WHERE u.name = :name")
+    Optional<User> findUserByUserName(@Param("name") String name);
+}
+
